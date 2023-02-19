181d1e1ce899d87ee712d72cf8a36807ede7a31c
pscieszka <piotr.scieszka.epinote@gmail.com>
2023-02-19 21:46:38 +0100

    api connected

--- Program.cs
+++ Program.cs
@@ -1,33 +1,30 @@
 ﻿using System;
-using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {   
         string api_path = "API_key.txt";
         string api_key = System.IO.File.ReadAllText(api_path); 
         Console.WriteLine($"Contents of {api_path} = {api_key}");
-
-    }
-}// lista miast wszystkich i jak nie ma to sprawdza algorytm podobienstwo znakow i sprawdza ktore miasto ma najwieksza podobienstwo 
-class Cities{
-    List<string> cities = new List<string>();
-    string cityInput(List<string> cities){
-        string city;
         Console.WriteLine("Enter city name: ");
-        city = Console.ReadLine();
-        if(cities.Contains(city)){
-            return city;
+        string? city = Console.ReadLine();
+
+        using (var client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Add("User-Agent", "MyConsoleApp/1.0");
+
+            HttpResponseMessage response = await client.GetAsync($"https://api.waqi.info/feed/{city}/?token={api_key}");
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(responseBody);
+            
         }
-        else{
-            string notFound = "City not found";
-            return notFound;
-        
     }
 }
 
-
-    
-
+// lista miast wszystkich i jak nie ma to sprawdza algorytm podobienstwo znakow i sprawdza ktore miasto ma najwieksza podobienstwo 
 
