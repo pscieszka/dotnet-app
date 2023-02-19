ffd0a49f4819d4b47ebdc8d039044e88819f72e6
pscieszka <piotr.scieszka.epinote@gmail.com>
2023-02-19 21:06:46 +0100

    cities

--- Program.cs
+++ Program.cs
@@ -10,4 +10,24 @@ class Program
         Console.WriteLine($"Contents of {api_path} = {api_key}");
 
     }
+}// lista miast wszystkich i jak nie ma to sprawdza algorytm podobienstwo znakow i sprawdza ktore miasto ma najwieksza podobienstwo 
+class Cities{
+    List<string> cities = new List<string>();
+    string cityInput(List<string> cities){
+        string city;
+        Console.WriteLine("Enter city name: ");
+        city = Console.ReadLine();
+        if(cities.Contains(city)){
+            return city;
+        }
+        else{
+            string notFound = "City not found";
+            return notFound;
+        
+    }
 }
+
+
+    
+
+
