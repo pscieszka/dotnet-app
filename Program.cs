755af3181c2a9def01a7ea5e0f79ee4f9f17f5ed
pscieszka <piotr.scieszka.epinote@gmail.com>
2023-02-19 17:44:29 +0100

    api

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {   
+        string api_path = "API_key.txt";
+        string api_key = System.IO.File.ReadAllText(api_path); 
+        Console.WriteLine($"Contents of {api_path} = {api_key}");
+
+    }
+}
