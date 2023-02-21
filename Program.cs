6fdb3ee341d70706a570e11691c98fda66729dbe
pscieszka <piotr.scieszka.epinote@gmail.com>
2023-02-21 15:48:55 +0100

    aqiQuality

--- Program.cs
+++ Program.cs
@@ -8,10 +8,30 @@ class Output
     public string responseBodyPrettier(string responseBody)
     {   
         string tempData = "";
+        string aqiTable ="";
         string[] responseBodyPrettier ={};
         for(int i=0; i<responseBody.Length; i++){
             if(responseBody[i]=='a' && responseBody[i+1]=='q' && responseBody[i+2]=='i'){
-                tempData = $"aqi={responseBody[i+5]}{responseBody[i+6]}";
+                int aqiQuality= ((int)responseBody[i+5] - '0')*10+((int)responseBody[i+6] - '0'); //what if aqiQuality is 100+? to fix
+                if(aqiQuality<50){
+                    aqiTable = "Air quality is good";
+                }
+                else if(aqiQuality<100){
+                    aqiTable = "Air quality is moderate";
+                }
+                else if(aqiQuality<150){
+                    aqiTable = "Air quality is unhealthy for sensitive groups"; 
+                }
+                else if(aqiQuality<200){
+                    aqiTable = "Air quality is unhealthy";
+                }
+                else if(aqiQuality<300){
+                    aqiTable = "Air quality is very unhealthy";
+                }
+                else if(aqiQuality>300){
+                    aqiTable = "Air quality is hazardous";
+                }
+                tempData = $"aqi={responseBody[i+5]}{responseBody[i+6]} - {aqiTable}";
                 break;
             }
         }
