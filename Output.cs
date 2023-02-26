c3c5321b88af021ed8df2a591cc34e6379f6c5aa
pscieszka <piotr.scieszka.epinote@gmail.com>
2023-02-26 16:59:56 +0100

    output almost finished

--- Output.cs
+++ Output.cs
@@ -42,14 +42,47 @@ namespace dotnet_app
         }
         return tempData;
     }
-    public string cleaner(string responseBody){ 
+    public string cleaner(string responseBody){
+        responseBody = responseBody.Replace("{", "").Replace("}", " ").Replace(":", " ").Replace(",", "").Replace("\"", "").Replace("v","").Replace("data", ""); 
         int index1 = responseBody.IndexOf("idx");
         int index2 = responseBody.IndexOf("iaqi");
-        responseBody =  responseBody.Substring(0,index1) + responseBody.Substring(index2,130);
-        responseBody = responseBody.Replace("{", "").Replace("}", " ").Replace(":", " ").Replace(",", "").Replace("\"", "").Replace("data","").Replace("v","");
+        responseBody = responseBody.Substring(0,index1)+ responseBody.Substring(index2,75);
+        string status="", aqi="", pm25="", pm10="";
+        for(int i=0; i<responseBody.Length; i++){
+            if(responseBody[i]=='s' && responseBody[i+1]=='t'){
+                i+=7;
+                while(responseBody[i]!=' '){
+                    status+=responseBody[i];
+                    i++;
+                }
+            }
+            if(responseBody[i-1]==' ' && responseBody[i]=='a'){
+                i+=3;
+                while(responseBody[i]!='i'){
+                    aqi+=responseBody[i];
+                    i++;
+                }
+            }
+            if(responseBody[i-1]=='p' && responseBody[i]=='m' && responseBody[i+1]=='1'){
+                i+=5;
+                while(responseBody[i]!=' '){
+                    pm10+=responseBody[i];
+                    i++;
+                }
+            }
+            if(responseBody[i-1]=='p' && responseBody[i]=='m' && responseBody[i+1]=='2'){
+                i+=5;
+                while(responseBody[i]!=' '){
+                    pm25+=responseBody[i];
+                    i++;
+                }
+            }
+
+           
+        }
+        responseBody = $"Status: {status}\nAqi:{aqi}\npm25: {pm25}\npm10: {pm10}";
         return responseBody;
     }
-    
     public bool checkIfWorks(string responseBody){
         if(responseBody.Contains("error")){
             return false;
@@ -60,13 +93,4 @@ namespace dotnet_app
     }
 }}
 
-// {"status":"ok","data":{"aqi":65,"idx":8689,"attributions":[{"url":"http://monitoring.krakow.pios.gov.pl/","name":"Regional Inspectorate for Environmental Protection in Krakow 
-// (WIOŚ - Wojewódzki Inspektorat Ochrony Środowiska w Krakowie)","logo":"poland-wios-krakowie.png"},{"url":"http://powietrze.gios.gov.pl/","name":"Główny inspektorat ochrony środowiska
-// ","logo":"poland-wios-national.png"},{"url":"https://waqi.info/","name":"World Air Quality Index Project"}],"city":{"geo":[50.057447,19.946008],"name":"Kraków-ul. Dietla, Małopolska, Poland
-// ","url":"https://aqicn.org/city/poland/malopolska/krakow-ul.-dietla","location":""},"dominentpol":"pm25","iaqi":{"co":{"v":0.1},"dew":{"v":6},"h":{"v":76},"no2":{"v":9.6},"o3":{"v":13.3},
-// "p":{"v":1002},"pm10":{"v":28},"pm25":{"v":65},"so2":{"v":1.7},"t":{"v":10},"w":{"v":7.7},"wg":{"v":13.3}},"time":{"s":"2023-02-24 14:00:00","tz":"+01:00","v":1677247200,
-// "iso":"2023-02-24T14:00:00+01:00"},"forecast":{"daily":{"o3":[{"avg":18,"day":"2023-02-24","max":24,"min":13},{"avg":22,"day":"2023-02-25","max":28,"min":15},{"avg":24,"day":
-// "2023-02-26","max":29,"min":20},{"avg":24,"day":"2023-02-27","max":29,"min":20},{"avg":19,"day":"2023-02-28","max":19,"min":19}],"pm10":[{"avg":22,"day":"2023-02-24","max":30,"min":13},
-// {"avg":11,"day":"2023-02-25","max":26,"min":6},{"avg":14,"day":"2023-02-26","max":23,"min":9},{"avg":20,"day":"2023-02-27","max":30,"min":12},{"avg":27,"day":"2023-02-28","max":28,"min":27}],
-// "pm25":[{"avg":70,"day":"2023-02-24","max":86,"min":50},{"avg":37,"day":"2023-02-25","max":76,"min":18},{"avg":46,"day":"2023-02-26","max":67,"min":27},{"avg":60,"day":"2023-02-27","max":80,
-// "min":44},{"avg":80,"day":"2023-02-28","max":83,"min":80}]}},"debug":{"sync":"2023-02-24T22:14:13+09:00"}}}
+//status ok aqi 25iaqi co  0.1 dew  -5 h  64 no2  6.5 o3  22.3 p  1014 pm10  7 pm25  25 so2  
--- Program.cs
+++ Program.cs
@@ -10,16 +10,15 @@ class Program
     static async Task Main(string[] args)
     {   
         string api_path = "API_key.txt";
-        string api_key = System.IO.File.ReadAllText(api_path); 
-        Console.WriteLine($"Contents of {api_path} = {api_key}");
-        //Console.WriteLine("Enter city name: ");
-        //string? city = Console.ReadLine();
+        string api_key = System.IO.File.ReadAllText(api_path); // API key from file
+        Console.WriteLine("Enter city name: ");
+        string? city = Console.ReadLine();
 
         using (var client = new HttpClient())
         {
             client.DefaultRequestHeaders.Add("User-Agent", "MyConsoleApp/1.0");
 
-            HttpResponseMessage response = await client.GetAsync($"https://api.waqi.info/feed/krakow/?token={api_key}");//client.GetAsync($"https://api.waqi.info/feed/{city}/?token={api_key}");
+            HttpResponseMessage response = await client.GetAsync($"https://api.waqi.info/feed/{city}/?token={api_key}");
             Output Output = new Output();
             string responseBody = await response.Content.ReadAsStringAsync();
             if(Output.checkIfWorks(responseBody)){
